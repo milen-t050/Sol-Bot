0b8c95b12869374a2999acc8b3e5b6fea7911cfd
Milen <milenthomas2020@gmail.com>
2024-05-03 00:54:17 -0400

    Finished working on the buying and selling function with solana tokens, maybe can work on the getTransaction method and how to work around it as we now have to wait 15 seconds for confirmation resposne, this is not ideal

--- order.py
+++ order.py
@@ -8,7 +8,7 @@ import solders
 from solders.keypair import Keypair
 from solders.transaction import VersionedTransaction 
 from solders.signature import Signature
-from solders.transaction import Legacy
+from solders.transaction import Transaction as LegacyTransaction
 from solders.transaction import TransactionError
 from solana.rpc.async_api import AsyncClient
 from solders.pubkey import Pubkey
@@ -34,18 +34,44 @@ CYAN = Fore.CYAN
 
 SLIPPAGE = 1000 #in BPS, this is 10% slippage
 SOL = "So11111111111111111111111111111111111111112"
-PRIORITY_FEE_LAMPORTS = 1000000
+PRIORITY_FEE_LAMPORTS = 5000
 KEY = p.sol_wallet_private_key
-BASE_URL = "https://public-api.birdeye.so/defi/"
 
 SWAP_MODE = "ExactIn", "ExactOut"
 JUPITER_QUOTE_ENDPOINT = "https://quote-api.jup.ag/v6/quote"
 JUPITER_SWAP_ENDPOINT = "https://quote-api.jup.ag/v6/swap"
 
 api_devnet = "https://api.devnet.solana.com"
+api_mainnet = "https://api.mainnet-beta.solana.com"
 MAX_RETRIES = 3
-CONFRIMATION_SLEEP_SECONDS = 0.5
-BANK_NOTE_COMMITMENT = "confirmed"
+CONFRIMATION_SLEEP_SECONDS = 15 #12 worked sometimes, safe side 15
+BANK_NOTE_COMMITMENT = "finalized"
+AS_LEGACY_TRANSACTION = True
+
+class ExceededSlippageThresholdError(Exception):
+    """Exception raised when the slippage threshold for a transaction is exceeded."""
+    pass
+
+class TransactionFailedError(Exception):
+    """Exception raised when a transaction fails to complete."""
+    pass
+
+class InsufficientFundsError(Exception):
+    """Exception raised when there are not enough funds to perform a transaction."""
+    pass
+
+class RPCException(Exception):
+    """Exception raised for RPC related errors."""
+    pass
+
+class UnconfirmedTxError(Exception):
+    """Exception raised when a transaction remains unconfirmed past a certain threshold."""
+    pass
+
+class TransactionExpiredBlockheightExceededError(Exception):
+    """Exception raised when a transaction is not confirmed before the last valid block height is exceeded."""
+    pass
+
 
 '''
 SIDENODE MAKE SURE TO HAVE AMOUNTS IN LAMPORTS
@@ -70,7 +96,8 @@ async def fetch_quote_response(
         "outputMint": output_mint,
         "amount": amount,
         "slippageBps": SLIPPAGE,
-        "swapMode": swap_mode
+        "swapMode": swap_mode,
+        "asLegacyTransaction": AS_LEGACY_TRANSACTION
     }
         
     async with httpx.AsyncClient() as client:
@@ -83,11 +110,10 @@ async def fetch_quote_response(
         print(e)
     
     if "error" in data.keys():
-        print(f"Error: {data['error']}")
-        print("oops")
+        print(f"{CYAN}Error: {data['error']}{RESET}")
+        
+    logWithTimestamp(f"{PURPLE}Quote response fetched successfully")
     return data
-
-asyncio.run(fetch_quote_response(SOL, "8XethNffiUbgXEsjqJcPZzJYkCw2Q44fhPuMrZsNaZda", 20, "ExactIn"))
 
 async def fetch_swap_transaction(
     input_mint: str,
@@ -100,11 +126,12 @@ async def fetch_swap_transaction(
     
     async with httpx.AsyncClient() as client:
         response = await client.post(JUPITER_SWAP_ENDPOINT, json={
-            "qouteResponse": quote_response,
+            "quoteResponse": quote_response,
             "userPublicKey": p.sol_wallet_public_key,
             "wrapAndUnwrapSol" : True,
             "dynamicComputeUnitLimit": True,
-            "prioritizationFeeLamports": PRIORITY_FEE_LAMPORTS
+            "prioritizationFeeLamports": PRIORITY_FEE_LAMPORTS,
+            "asLegacyTransaction": AS_LEGACY_TRANSACTION,
         })
     
     await client.aclose()
@@ -115,8 +142,8 @@ async def fetch_swap_transaction(
         print(e)
     
     if "error" in data.keys():
-        print(f"Error: {data['error']}")
-        
+        print(f"{CYAN}Error: {data['error']}{RESET}")
+     
     transaction_bytes = base64.b64decode(data['swapTransaction'])
     
     quote = {
@@ -124,8 +151,8 @@ async def fetch_swap_transaction(
         "outAmount": int(quote_response['outAmount']),
         "priceImpactPct": float(quote_response['priceImpactPct']) # percentage
     }
-    print(quote)
-    print(transaction_bytes)
+    
+    logWithTimestamp(f"{PURPLE}Swap transaction fetched successfully")
     
     return transaction_bytes, quote
 
@@ -134,52 +161,59 @@ async def create_swap_txn(
     output_mint: str,
     amount: int,
     swap_mode: str=SWAP_MODE
-) -> Tuple[Union[VersionedTransaction], Dict[str, Any]]:
+) -> Tuple[Union[LegacyTransaction], Dict[str, Any]]:
     
     transaction_bytes, quote = await fetch_swap_transaction(input_mint, output_mint, amount, swap_mode)
     
-    txn = VersionedTransaction.from_bytes(transaction_bytes)
+    txn = LegacyTransaction.from_bytes(transaction_bytes)
+    
+    logWithTimestamp(f"{PURPLE}Transaction created successfully")
     
     return txn, quote
 
 async def send_txn(
     client: AsyncClient,
-    txn: Union[Transaction, VersionedTransaction],
+    txn: Union[Transaction, LegacyTransaction],
     signer: Keypair,
     max_retries: int=MAX_RETRIES,
     confirmation_sleep_seconds: float=0.5
     ) -> Signature:
     
-    txn = Transaction.from_solders(txn) if isinstance(txn, Legacy) else txn
+    txn = Transaction.from_solders(txn) if isinstance(txn, LegacyTransaction) else txn
     num_retires = 0
     while num_retires < max_retries:
         try:
             blockhash_resp = await client.get_latest_blockhash(commitment=BANK_NOTE_COMMITMENT)
             recent_blockhash = blockhash_resp.value.blockhash
             last_valid_block_height = blockhash_resp.value.last_valid_block_height
-            if not isinstance(txn, VersionedTransaction):
-                txn_response = await client.send_transaction(txn, signer, recent_blockhash=recent_blockhash)
-            else:
-                txn = change_blockhash_of_versioned_transaction(txn, signer=signer, new_blockhash=recent_blockhash)
-                txn_response = await client.send_transaction(txn)
-            txn_signature = txn_response.value
             
-            if txn_signature:
-                confirmation_resp =  await client.confirm_transaction(
-                    txn_signature,
-                    commitment=BANK_NOTE_COMMITMENT,
-                    sleep_seconds=confirmation_sleep_seconds
-                    last_valid_block_height=last_valid_block_height
+            tx_opts = TxOpts(
+                skip_preflight=True,
+                preflight_commitment=BANK_NOTE_COMMITMENT,
+                max_retries=max_retries
+                             )
+            
+            txn_response = await client.send_transaction(txn, signer, recent_blockhash=recent_blockhash)
+            txn_sig = txn_response.value
+            
+            await asyncio.sleep(confirmation_sleep_seconds)
+            
+            if txn_sig:
+                confirmation_resp =  await client.get_transaction(
+                    txn_sig,
+                    commitment="confirmed",
                 )
                 
-                confirmation_json_resp = json.loads(confirmation_resp.value[0].to_json())
-                if (
-                    "status" in confirmation_json_resp.keys() and
-                    "Ok" in confirmation_json_resp["status"].keys()
-                ): raise Exception("Transaction failed")
-                return txn_signature
+                confirmation_json_resp = json.loads(confirmation_resp.value.to_json())
+                
+                if confirmation_json_resp.get('value', {}).get('transaction', {}).get('meta', {}).get('status', {}) is None:
+                    raise Exception(f"{CYAN}Transaction failed{RESET}")
+                
+                logWithTimestamp(f"{PURPLE}Transaction confirmed successfully")
+                
+                return txn_sig
         
-        except (UnconfimredTxError, TransactionExpiredBlockheightExceededError) as e:
+        except (UnconfirmedTxError, TransactionExpiredBlockheightExceededError) as e:
             num_retires += 1
             continue
         
@@ -188,20 +222,19 @@ async def send_txn(
                 num_retires += 1
                 continue
             elif "0x1" in str(e) and "insufficent" in str(e):
-                raise InsufficentFunndsError(f"Insufficent funds to complete transaction: {e}")
+                raise InsufficientFundsError(f"{CYAN}Insufficent funds to complete transaction: {e}{RESET}")
             elif "0x1771" in str(e):
-                raise ExceededSlippageThresholdError(f"Exceeded slippage threshold: {e}")
+                raise ExceededSlippageThresholdError(f"{CYAN}Exceeded slippage threshold: {e}{RESET}")
             raise TransactionFailedError(e)
         
-        raise TransactionFailedError("Transaction failed")
+        raise TransactionFailedError(f"{CYAN}Transaction failed{RESET}")
+    logWithTimestamp(f"{CYAN}Transaction failed")
 
 
 async def swap_spl_tokens(
     client: AsyncClient,
-    sender_private_key: str,
     output_mint: Union[str, Keypair],
     input_token_amount: int,
-    slippage: int,
     input_mint: str,
     swap_mode: str=SWAP_MODE  
 ) -> Tuple[Signature, Dict[str, Any]]:
@@ -220,6 +253,7 @@ async def swap_spl_tokens(
         output_mint_pubkey = Pubkey.from_string(output_mint)
         supply_resp =  await client.get_token_supply(output_mint_pubkey, commitment=BANK_NOTE_COMMITMENT)
         output_decimals = json.loads(supply_resp.value.to_json())["decimals"]
+        logWithTimestamp(f"{GREEN}Getting the route for a swap, we are buying the SPL token...")
 
     #Sell the SPL token
     elif input_mint != SOL and output_mint == SOL:
@@ -227,12 +261,12 @@ async def swap_spl_tokens(
         input_mint_pubkey = Pubkey.from_string(input_mint)
         supply_resp =  await client.get_token_supply(input_mint_pubkey, commitment=BANK_NOTE_COMMITMENT)
         input_decimals = json.loads(supply_resp.value.to_json())["decimals"]
+        logWithTimestamp(f"{RED}Getting the route for a swap, we are selling the SPL token...")
     
     else:
-        raise Exception("We can only swap SOL pairs...")
+        raise Exception(f"{CYAN}We can only swap SOL pairs...")
     
     amount = math.ceil(input_token_amount * (10**input_decimals))
-    priority_fee_lamports = math.ceil(PRIORITY_FEE_LAMPORTS * (10**output_decimals))
     
     #slippage is in BPS (Basis Points)
     slippage = SLIPPAGE
@@ -243,149 +277,27 @@ async def swap_spl_tokens(
         client,
         txn=txn,
         signer=owner_keypair,
-        max_retries=MAX_RETRIES
+        max_retries=MAX_RETRIES,
         confirmation_sleep_seconds=CONFRIMATION_SLEEP_SECONDS
     )
     
     quote["inAmount"] = quote["inAmount"] / (10**input_decimals)
     quote["outAmount"] = quote["outAmount"] / (10**output_decimals)
     
+    logWithTimestamp(f"{BLUE}InAmount: {quote['inAmount']}, OutAmount: {quote['outAmount']}, Price Impact: {quote['priceImpactPct']}%{RESET}")
+    
     return txn_signature, quote
 
-
+async def main(
+    intput_mint: str,
+    input_token_amount: float,
+    output_mint: str,
+    swap_mode: str=SWAP_MODE  
+):
     
-    
-    
-    
+    async with AsyncClient(p.http_provider) as client:
+        txn_signature, quote = await swap_spl_tokens(client, output_mint, input_token_amount, intput_mint, swap_mode)
+        logWithTimestamp(f"{BLUE}Transaction completed successfully")
         
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-# def token_price(coin_address):
-
-#     url = f"{BASE_URL}price?address={coin_address}"
-
-#     headers = {"x-chain": "solana", "X-APi-KEY": p.birdEye_apiKey}
-
-#     response = requests.get(url, headers=headers)
-
-#     price_data = response.json()
-
-#     if price_data['success']:
-#         return price_data['data']['value']
-#     else:
-#         return None
-
-# def get_sol_price_usd():
-#     # Example using a public API - replace with your choice of API
-#     response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd')
-#     data = response.json()
-#     return data['solana']['usd']
-
-# def calculate_lamports_for_dollar(dollar_amount):
-#     sol_price = get_sol_price_usd()
-#     sol_amount = dollar_amount / sol_price
-#     lamports = sol_amount * 1_000_000_000
-#     return int(lamports)
-
-# def Buy(token, amount):
-    
-#     http_client = Client("https://api.mainnet-beta.solana.com")
-    
-#     token_pric = token_price(token)
-    
-#     logWithTimestamp("Getting the route for a swap")
-#     quote = requests.get(f'https://quote-api.jup.ag/v6/quote?inputMint={SOL}&outputMint={token}&amount={amount}&slippage={SLIPPAGE}').json()
-
-#     logWithTimestamp(quote)
-    
-#     logWithTimestamp("Getting the serialized transaction to swap")
-    
-#     txRes = requests.post("https://quote-api.jup.ag/v6/swap", headers={"Content-Type": "application/json"}, data=json.dumps({"quoteResponse": quote, "userPublicKey": p.sol_wallet_public_key})).json()
-#     print(txRes)
-    
-#     logWithTimestamp("Deserializing the transaction")
-#     swapTx = base64.b64decode(txRes['swapTransaction'])
-#     tx1 = VersionedTransaction.from_bytes(swapTx)
-#     tx2 = VersionedTransaction(tx1.message, [KEY])
-    
-#     logWithTimestamp("Executing the swap")
-#     txId = http_client.send_raw_transaction(bytes(tx), TxOpts(skip_preflight=True)).value
-#     logWithTimestamp(f"https://solscan.io/tx/{str(txId)}")
-    
-#     logWithTimestamp("Checking transaction status...")
-#     get_transaction_details = http_client.confirm_transaction(tx_sig=Signature.from_string(str(txId)), sleep_seconds=1)
-#     transaction_status = get_transaction_details.value[0].err
-
-#     if transaction_status is None:
-#         logWithTimestamp("Transaction SUCCESS!")
-#     else:
-#         logWithTimestamp(f"{RED}! Transaction FAILED!{RESET}")
-    
-
-# amount = calculate_lamports_for_dollar(1)
-# print(f"Amount in lamports for $1: {amount}")
-# Buy("8XethNffiUbgXEsjqJcPZzJYkCw2Q44fhPuMrZsNaZda", amount)
+# example use case for sell asyncio.run(main("8XethNffiUbgXEsjqJcPZzJYkCw2Q44fhPuMrZsNaZda", 7220, SOL, "ExactIn"))  
+# example use case for buy asyncio.run(main(SOL, 0.0001, "8XethNffiUbgXEsjqJcPZzJYkCw2Q44fhPuMrZsNaZda", "ExactOut"))
